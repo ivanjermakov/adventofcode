b3e95d52fb3ee2115f00052a2459cb5f60a29a58
ivanjermakov <ivanjermakov1@gmail.com>
2021-12-25 12:51:14 +0100

    Day 2

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,10 @@
 module Main where
-import Day1
+
+import Day2
 
 main :: IO ()
 main = do
-   content <- readFile "resources/d1.txt"
-   let depths = map (\w -> read w :: Int) (words content)
-   let res = getDepthIncreaseCountWindow depths
-   print res
+  content <- readFile "resources/d2.txt"
+  let plan = lines content
+  let res = calculateFinalPosition plan
+  print res
--- src/Day2.hs
+++ src/Day2.hs
@@ -1,2 +1,54 @@
 module Day2 where
 
+import Data.Char
+import Data.Maybe
+
+data Direction = Forward | Up | Down
+  deriving (Show, Read)
+
+data Step = Step Direction Int
+  deriving (Show)
+
+data Position = Position Horiz Depth Aim
+  deriving (Show)
+
+type Horiz = Int
+
+type Depth = Int
+
+type Aim = Int
+
+type RawPlan = [String]
+
+type Plan = [Step]
+
+calculateFinalPosition :: RawPlan -> Position
+calculateFinalPosition plan = foldToFinal $ fromMaybe [] (parsePlan plan)
+
+foldToFinal :: Plan -> Position
+foldToFinal = foldl (flip nextPosition) (Position 0 0 0)
+
+nextPosition :: Step -> Position -> Position
+nextPosition step (Position h d a) = case step of
+  Step Forward v -> Position (h + v) (d + a * v) a
+  Step Up v -> Position h d (a - v)
+  Step Down v -> Position h d (a + v)
+
+parsePlan :: RawPlan -> Maybe Plan
+parsePlan plan =
+  let comp = sequence . filter isJust . map parseStep
+   in comp plan
+
+parseStep :: String -> Maybe Step
+parseStep step = case Prelude.words step of
+  [direction, value] -> case parseDirection direction of
+    Just d -> Just $ Step d (read value :: Int)
+    Nothing -> Nothing
+  _ -> Nothing
+
+parseDirection :: String -> Maybe Direction
+parseDirection ws = case map toLower ws of
+  "forward" -> Just Forward
+  "up" -> Just Up
+  "down" -> Just Down
+  _ -> Nothing
