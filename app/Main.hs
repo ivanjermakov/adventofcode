1f42aa1b37a3f793b41286d01ab532a06a9624fd
ivanjermakov <ivanjermakov1@gmail.com>
2021-12-26 20:15:17 +0100

    Day 5.2

--- app/Main.hs
+++ app/Main.hs
@@ -6,5 +6,4 @@ main :: IO ()
 main = do
   content <- readFile "resources/d5.txt"
   let lines' = parse content
---print lines'
   print $ getCount lines'
--- src/Day5.hs
+++ src/Day5.hs
@@ -2,14 +2,15 @@
 
 module Day5 where
 
-import Data.List
 import Data.List.Split
+import Data.Map (fromListWith, toList)
+import Data.Tuple (swap)
 
 type Line = (Point, Point)
 
 type Point = (Int, Int)
 
-data Axis = Vertical | Horizontal | Diagonal
+data Axis = N | NE | E | SE
   deriving (Show, Enum)
 
 parse :: String -> [Line]
@@ -24,65 +25,55 @@ parsePoint :: String -> Point
 parsePoint content = toTuple . map read $ splitOn "," content
   where
     toTuple [a, b] = (a, b)
-    
+
 getCount :: [Line] -> Int
-getCount lines' = length $ getPoints lines'
+getCount line = length $ getIntersectPoints line
 
-getPoints :: [Line] -> [Point]
-getPoints lines' = nub . concatMap getPoints' $ pairs lines'
+getIntersectPoints :: [Line] -> [Point]
+getIntersectPoints = map fst . filter ((> 1) . snd) . dict . concatMap getAllPoints
 
-pairs :: Eq a => [a] -> [(a, a)]
-pairs xs = [(x1, x2) | x1 <- xs, x2 <- xs, x1 /= x2]
+getAllPoints :: Line -> [Point]
+getAllPoints line = case axis line of
+  N -> getNPoints line
+  E -> getEPoints line
+  NE -> getNePoints line
+  SE -> getSePoints line
 
-getPoints' :: (Line, Line) -> [Point]
-getPoints' (l1, l2) = case (axis l1, axis l2) of
-  (Vertical, Horizontal) -> getIntersectPoints' l1 l2
-  (Horizontal, Vertical) -> getIntersectPoints' l2 l1
-  (Diagonal, _) -> []
-  (_, Diagonal) -> []
-  (a, _) -> getCoincidentPoints a l1 l2
+getNPoints :: Line -> [Point]
+getNPoints ((x, y1), (_, y2)) = map (x,) $ rangeToList (y1, y2)
 
---first line is vertical
-getIntersectPoints' :: Line -> Line -> [Point]
-getIntersectPoints' l1 l2 =
-  let (((x1, _), (y1min, y1max)), ((x2min, x2max), (y2, _))) = (getRanges l1, getRanges l2)
-   in if x1 `inRange` (x2min, x2max) && y2 `inRange` (y1min, y1max) then [(x1, y2)] else []
+getEPoints :: Line -> [Point]
+getEPoints ((x1, y), (x2, _)) = map (,y) $ rangeToList (x1, x2)
 
-getCoincidentPoints :: Axis -> Line -> Line -> [Point]
-getCoincidentPoints a l1 l2 = case a of
-  Vertical -> case (fst (fst l1) == fst (fst l2), getIntersectingRange l1ys l2ys) of
-    (True, Just vr) -> map (fst l1xs,) $ rangeToList vr
-    _ -> []
-  Horizontal -> case (snd (fst l1) == snd (fst l2), getIntersectingRange l1xs l2xs) of
-    (True, Just hr) -> map (,fst l1ys) $ rangeToList hr
-    _ -> []
-  Diagonal -> []
+getNePoints :: Line -> [Point]
+getNePoints line = ps
   where
-    (l1xs, l1ys) = getRanges l1
-    (l2xs, l2ys) = getRanges l2
+    ((x1, y1), (x2, y2)) = normalize line
+    ps = zip (rangeToList (x1, x2)) (reverse $ rangeToList (y1, y2))
+
+getSePoints :: Line -> [Point]
+getSePoints line = ps
+  where
+    ((x1, y1), (x2, y2)) = normalize line
+    ps = zip (rangeToList (x1, x2)) (rangeToList (y1, y2))
+
+normalize :: Line -> Line
+normalize line = if x1 < x2 then swap line else line
+  where
+    ((x1, _), (x2, _)) = line
 
 axis :: Line -> Axis
 axis ((x1, y1), (x2, y2))
-  | x1 == x2 = Vertical
-  | y1 == y2 = Horizontal
-  | otherwise = Diagonal
+  | x1 == x2 = N
+  | y1 == y2 = E
+  | (x1 < x2 && y1 > y2) || (x1 > x2 && y1 < y2) = NE
+  | otherwise = SE
 
-orthogonal :: Line -> Line -> Bool
-orthogonal a b = (1 ==) $ sum . map (fromEnum . axis) $ [a, b]
-
-getRanges :: Line -> Line
-getRanges ((x1, y1), (x2, y2)) = ((minimum xs, maximum xs), (minimum ys, maximum ys))
-  where
-    xs = [x1, x2]
-    ys = [y1, y2]
-
-inRange :: Ord a => a -> (a, a) -> Bool
-inRange x (a, b) = x >= a && x <= b
-
-getIntersectingRange :: (Num a, Ord a) => (a, a) -> (a, a) -> Maybe (a, a)
-getIntersectingRange (a1, a2) (b1, b2) = if b - a + 1 > 0 then Just (a, b) else Nothing
-  where
-    (a, b) = (maximum [a1, b1], minimum [a2, b2])
+dict :: (Ord a) => [a] -> [(a, Integer)]
+dict = toList . fromListWith (+) . map (,1)
 
 rangeToList :: (Int, Int) -> [Int]
-rangeToList (a, b) = take (b - a + 1) $ iterate (+ 1) a
+rangeToList r = take (b - a + 1) $ iterate (+ 1) a
+  where
+    l = [fst r, snd r]
+    (a, b) = (minimum l, maximum l)
