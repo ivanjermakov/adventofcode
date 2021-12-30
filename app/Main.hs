aa9e674f75e9f98e7708b88c8e985b7d9eb28a35
ivanjermakov <ivanjermakov1@gmail.com>
2021-12-30 04:27:10 +0100

    Day 9.2 optimized

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,13 @@
 module Main where
 
-import Day9
 import Data.List (sort)
+import Data.Matrix (prettyMatrix, (!))
+import Day9
 
 main :: IO ()
 main = do
-  content <- readFile "resources/d9-test.txt"
+  content <- readFile "resources/d9.txt"
   let m = parse content
   let lps = getLowest m
-  let bs = product . take 3 . reverse . sort . map (length . getBasin m) $ lps
+  let bs = product . take 3 . reverse . sort . map (length . basin m) $ lps
   print bs
--- src/Day9.hs
+++ src/Day9.hs
@@ -2,46 +2,50 @@ module Day9 where
 
 import Control.Monad (liftM2)
 import Data.List (nub)
-import Data.Maybe (catMaybes, fromJust, isJust)
-
-type Matrix = [[Int]]
+import Data.Matrix (Matrix, fromLists, ncols, nrows, safeGet, (!))
+import Data.Maybe (catMaybes)
 
 type Pos = (Int, Int)
 
-parse :: String -> Matrix
-parse = map (map $ read . pure) . lines
-
-getBasin :: Matrix -> Pos -> [Pos]
-getBasin = getBasin' []
-
-getBasin' :: [Pos] -> Matrix -> Pos -> [Pos]
-getBasin' ps m curr = nub $ ps ++ concatMap (getBasin' newPs m) ns
+parse :: String -> Matrix Int
+parse = fromLists . ls
   where
-    newPs = ps ++ ns
-    ns = filter (\p -> fromJust (p `at` m) /= 9) . filter (`notElem` ps) . filter (isJust . flip at m) . neighbourPositions $ curr
+    ls = map (map $ (\e -> read e :: Int) . pure) . lines
 
-getLowest :: Matrix -> [Pos]
+basin :: Matrix Int -> Pos -> [Pos]
+basin = basin' []
+
+basin' :: [Pos] -> Matrix Int -> Pos -> [Pos]
+basin' visited m curr = nub $ curr : nsPos
+  where
+    nsPos = foldr (\n acc -> acc ++ basin' acc m n) newPs ns
+    ns = unvisitedNeighbours visited m curr
+    newPs = curr : visited
+
+unvisitedNeighbours :: [Pos] -> Matrix Int -> Pos -> [Pos]
+unvisitedNeighbours visited m curr =
+  filter (\p -> p `inBounds` shape m && p `notElem` (curr : visited) && (m ! p) /= 9)
+    . neighbourPositions
+    $ curr
+
+inBounds :: Pos -> (Int, Int) -> Bool
+inBounds (i, j) (m, n) = i > 0 && i <= m && j > 0 && j <= n
+
+shape :: Matrix a -> (Int, Int)
+shape m = (nrows m, ncols m)
+
+getLowest :: Matrix Int -> [Pos]
 getLowest matrix = filter (`isLowest` matrix) ps
   where
-    m = length matrix
-    n = length . head $ matrix
+    (m, n) = shape matrix
     ps = liftM2 (,) (nat m) (nat n)
-    nat a = take a $ iterate (+ 1) 0
+    nat a = take a $ iterate (+ 1) 1
 
-isLowest :: Pos -> Matrix -> Bool
+isLowest :: Pos -> Matrix Int -> Bool
 isLowest (i, j) m = null neighbours
   where
-    (Just e) = at (i, j) m
-    neighbours = filter (<= e) . catMaybes $ (`at` m) <$> neighbourPositions (i, j)
+    e = m ! (i, j)
+    neighbours = filter (<= e) . catMaybes $ (\(i', j') -> safeGet i' j' m) <$> neighbourPositions (i, j)
 
 neighbourPositions :: Pos -> [Pos]
 neighbourPositions (i, j) = [(i - 1, j), (i, j + 1), (i + 1, j), (i, j - 1)]
-
-at :: Pos -> Matrix -> Maybe Int
-at (i, j) m = (m !? i) >>= (!? j)
-
-(!?) :: [a] -> Int -> Maybe a
-(!?) l i
-  | i < 0 = Nothing
-  | i < length l = Just $ l !! i
-  | otherwise = Nothing
