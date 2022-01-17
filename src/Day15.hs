6ae0ed21bba49e3b64cd8abfc2aa338ca638edd3
ivanjermakov <ivanjermakov1@gmail.com>
2022-01-17 04:22:34 +0100

    Day 15.2

--- src/Day15.hs
+++ src/Day15.hs
@@ -2,77 +2,69 @@ module Day15 where
 
 import Data.Bifunctor (second)
 import Data.Char (digitToInt)
-import Data.List (sortOn)
+import Data.Map (Map)
 import qualified Data.Map as M
-import Data.Matrix (Matrix, fromLists, mapPos, ncols, nrows, safeGet, toList, (!))
+import Data.Matrix (Matrix)
+import qualified Data.Matrix as MT
 import Data.Maybe (fromJust, isJust)
+import Data.PQueue.Min (MinQueue)
+import qualified Data.PQueue.Min as Q
 
 type Grid = Matrix Int
 
 type Pos = (Int, Int)
 
-data Node = Node {risk :: Int, tentative :: Int, visited :: Bool, prev :: Maybe Pos}
-  deriving (Show)
+type Queue = MinQueue (Int, Pos)
 
-type NodeMap = M.Map Pos Node
+type VisitedMap = Map Pos Bool
 
 main15 :: IO ()
 main15 = do
   content <- readFile "resources/d15.txt"
-  let g = parse content
-  let m = nodeMap g
-  let resM = traverseMap g m
-  let lastPos = (nrows g, ncols g)
-  print $ tentative $ resM M.! lastPos
+  let pg = parse content
+  let g = expand (5, 5) pg
+  let q = Q.fromList [(0, (1, 1))]
+  let m = M.fromList . MT.toList . MT.mapPos (\p _ -> (p, False)) $ g
+  let dist = distance (g, q, m)
+  print dist
 
-traverseMap :: Grid -> NodeMap -> NodeMap
-traverseMap g m = traverseMap' startPos g newM
+expand :: (Int, Int) -> Grid -> Grid
+expand (i, j) g = expand' (MT.<->) firstRow (steps j)
   where
-    startPos = (1, 1)
-    newM = M.adjust (\n -> n {tentative = 0}) startPos m
-
-traverseMap' :: Pos -> Grid -> NodeMap -> NodeMap
-traverseMap' p g m = case next of
-  (nPos, _) : _ -> traverseMap' nPos g newM
-  [] -> m
-  where
-    c = newM M.! p
-    adj = adjacent p g
-    newM = foldl (\accM (p', w') -> examineAdjacent p' (tentative c + w') p accM) (visitNode p m) adj
-    next = sortOn (tentative . snd) . filter (\(_, v) -> not . visited $ v) . M.toList $ newM
-
-examineAdjacent :: Pos -> Int -> Pos -> NodeMap -> NodeMap
-examineAdjacent p w prevP = M.adjust upd p
-  where
-    shouldUpdate v = not (visited v) && w < tentative v
-    upd v =
-      if shouldUpdate v
-        then v {tentative = w, prev = Just prevP}
-        else v
-
-visitNode :: Pos -> NodeMap -> NodeMap
-visitNode = M.adjust upd
-  where
-    upd v = v {visited = True}
+    firstRow = expand' (MT.<|>) g $ steps i
+    expand' f g' = foldl f g' . tail . scanl (\acc _ -> inc acc) g'
+    steps n = [1 .. n - 1]
+    inc = MT.mapPos (\_ v -> (v `mod` 9) + 1)
 
 parse :: String -> Grid
-parse = fromLists . map (map digitToInt) . lines
+parse = MT.fromLists . map (map digitToInt) . lines
 
-pretty :: (Show k, Show v) => M.Map k v -> String
-pretty = unlines . map (\(k, v) -> show k ++ " -> " ++ show v) . M.toList
+distance :: (Grid, Queue, VisitedMap) -> Int
+distance (g, q, m) =
+  if p == lastPos
+    then t
+    else distance (g, q'', vm)
+  where
+    lastPos = (MT.nrows g, MT.ncols g)
+    (q', (t, p)) = popNotVisited m q
+    adj = filter (not . (M.!) vm . fst) . adjacent p $ g
+    q'' = foldl (\acc (ap, at) -> Q.insert (t + at, ap) acc) q' adj
+    vm = M.adjust (const True) p m
 
-nodeMap :: Grid -> NodeMap
-nodeMap = M.fromList . map (\(p, r) -> (p, Node r infinity False Nothing)) . toList . mapPos (,)
+popNotVisited :: VisitedMap -> Queue -> (Queue, (Int, Pos))
+popNotVisited m q =
+  if m M.! p
+    then popNotVisited m q'
+    else (q', (t, p))
+  where
+    ((t, p), q') = Q.deleteFindMin q
 
 adjacent :: Pos -> Grid -> [(Pos, Int)]
 adjacent p g =
   map (second fromJust)
     . filter (\(_, m) -> isJust m)
-    . map ((\(i, j) -> ((i, j), safeGet i j g)) . tmap (+) p)
+    . map ((\(i, j) -> ((i, j), MT.safeGet i j g)) . tmap (+) p)
     $ [(-1, 0), (0, 1), (1, 0), (0, -1)]
 
 tmap :: (a -> b -> c) -> (a, a) -> (b, b) -> (c, c)
 tmap f (a, b) (c, d) = (f a c, f b d)
-
-infinity :: Int
-infinity = maxBound
