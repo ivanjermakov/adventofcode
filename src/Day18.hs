cc900352d18f5bf33707f7ceb9338c272fcbe132
ivanjermakov <ivanjermakov1@gmail.com>
2022-01-20 00:20:49 +0100

    Day 18.1 findExplode

--- src/Day18.hs
+++ src/Day18.hs
@@ -1,12 +1,17 @@
-{-# LANGUAGE TupleSections #-}
-
 module Day18 where
 
-import Data.Char (digitToInt)
+import Data.Char (digitToInt, isNumber)
 import Data.List (intercalate)
+import Data.Maybe (catMaybes, fromJust, listToMaybe, mapMaybe)
 import Data.Tree
+import qualified Data.Tree.Zipper as Z
 
 type NTree = Tree (Maybe Int)
+
+type NTreePos = Z.TreePos Z.Full (Maybe Int)
+
+data Side = LeftSide | RightSide | TopSide
+  deriving (Eq, Show)
 
 main18 :: IO ()
 main18 = do
@@ -30,17 +35,25 @@ parse :: String -> NTree
 parse = unfoldTree f
   where
     f :: String -> (Maybe Int, [String])
-    f [n] = (Just $ digitToInt n, [])
-    f s = (Nothing, [tail l, tail . init $ r])
+    f s
+      | all isNumber s = (Just $ read s, [])
+      | otherwise = (Nothing, [l, r])
       where
-        (l, r) = splitAt (midAt (0, 0, s)) s
+        (l, r) = splitPair s
+
+splitPair :: String -> (String, String)
+splitPair s = (l, r)
+  where
+    (l, r) = case splitAt (midAt (0, 0, s)) s of
+      (_ : l', _ : r') -> (l', init r')
+      _ -> error (show s)
 
 midAt :: (Int, Int, String) -> Int
 midAt (i, c, '[' : ss) = midAt (i + 1, c + 1, ss)
 midAt (i, c, ']' : ss) = midAt (i + 1, c - 1, ss)
 midAt (i, c, ',' : ss) = if c == 1 then i else midAt (i + 1, c, ss)
 midAt (i, c, _ : ss) = midAt (i + 1, c, ss)
-midAt (i, c, []) = -1
+midAt _ = -1
 
 toPairRepr :: NTree -> String
 toPairRepr = foldTree f
@@ -50,3 +63,114 @@ toPairRepr = foldTree f
 
 pretty :: NTree -> String
 pretty = drawTree . fmap show
+
+nearestLeft :: NTreePos -> Maybe NTreePos
+nearestLeft = find Nothing
+  where
+    find :: Maybe Side -> NTreePos -> Maybe NTreePos
+    find mcf n = case (mcf, Z.isLeaf n, Z.isRoot n) of
+      -- not first, leaf, not root
+      (Just cf, True, False) -> case cf of
+        TopSide -> Just n
+      -- not first, leaf, root
+      (Just _, True, True) -> Nothing
+      -- not first, node, not root
+      (Just cf, False, False) -> case cf of
+        LeftSide -> up n
+        RightSide -> lt n
+        TopSide -> rt n
+      -- not first, node, root
+      (Just cf, False, True) -> case cf of
+        LeftSide -> Nothing
+        RightSide -> lt n
+      -- first, leaf
+      (Nothing, True, _) -> up n
+      where
+        up n' = find (Just $ side n') (parentUnsafe n')
+        lt n' = find (Just TopSide) (leftUnsafe n')
+        rt n' = find (Just TopSide) (rightUnsafe n')
+
+nearestRight :: NTreePos -> Maybe NTreePos
+nearestRight = find Nothing
+  where
+    find :: Maybe Side -> NTreePos -> Maybe NTreePos
+    find mcf n = case (mcf, Z.isLeaf n, Z.isRoot n) of
+      -- not first, leaf, not root
+      (Just cf, True, False) -> case cf of
+        TopSide -> Just n
+      -- not first, leaf, root
+      (Just _, True, True) -> Nothing
+      -- not first, node, not root
+      (Just cf, False, False) -> case cf of
+        LeftSide -> rt n
+        RightSide -> up n
+        TopSide -> lt n
+      -- not first, node, root
+      (Just cf, False, True) -> case cf of
+        LeftSide -> rt n
+        RightSide -> Nothing
+      -- first, leaf
+      (Nothing, True, _) -> up n
+      where
+        up n' = find (Just $ side n') (parentUnsafe n')
+        lt n' = find (Just TopSide) (leftUnsafe n')
+        rt n' = find (Just TopSide) (rightUnsafe n')
+
+{-
+findExplodeNode :: Node -> Maybe Node
+findExplodeNode (Node (l, r) _) = head . filter isJust . map findExplodeNode $ [l, r]
+findExplodeNode (Leaf _ Nothing) = Nothing
+findExplodeNode (Leaf _ (Just p)) = if isExplode p then Just p else Nothing
+  where
+    isExplode n = ((> 3) . depth $ n) && pairNode n
+
+explode :: Node -> (Node, Bool)
+explode n = case mEn of
+  Nothing -> (n, True)
+  Just en -> (nr, False)
+    where
+      (Node (Leaf lv _, Leaf rv _) _) = en
+      n' = maybe n (updateNode (Leaf 0)) (Just en)
+      nl = maybe n' (updateLeaf (+ lv)) (nearestLeft n')
+      nr = maybe nl (updateLeaf (+ rv)) (nearestRight nl)
+  where
+    mEn = findExplodeNode n
+-}
+
+explode :: NTreePos -> (Bool, NTreePos)
+explode n = undefined
+
+findExplode :: NTreePos -> Maybe NTreePos
+findExplode n = case Z.isLeaf n of
+  False -> listToMaybe . mapMaybe findExplode . catMaybes $ [Z.firstChild n, Z.lastChild n]
+  True -> if isExplode p then Just p else Nothing
+    where
+      p = parentUnsafe n
+      isExplode n' = ((> 3) . depth $ n') && pairNode n'
+
+pairNode :: NTreePos -> Bool
+pairNode n = maybe False Z.isLeaf (Z.firstChild n) && maybe False Z.isLeaf (Z.lastChild n)
+
+depth :: NTreePos -> Int
+depth = depth' 0
+  where
+    depth' d n = if Z.isRoot n then d else depth' (d + 1) (parentUnsafe n)
+
+parentUnsafe :: NTreePos -> NTreePos
+parentUnsafe = fromJust . Z.parent
+
+leftUnsafe :: NTreePos -> NTreePos
+leftUnsafe = fromJust . Z.firstChild
+
+rightUnsafe :: NTreePos -> NTreePos
+rightUnsafe = fromJust . Z.lastChild
+
+side :: NTreePos -> Side
+side n
+  | Z.isRoot n = TopSide
+  | Z.isFirst n = LeftSide
+  | Z.isLast n = RightSide
+  | otherwise = error "side is unknown"
+
+transform :: (NTreePos -> NTreePos) -> NTree -> NTree
+transform f = Z.tree . f . Z.fromTree
--- test/Day18Spec.hs
+++ test/Day18Spec.hs
@@ -1,11 +1,22 @@
 module Day18Spec where
 
+import Control.Arrow ((>>>))
+import Data.Tree.Zipper
 import Day18
 import Test.Hspec
 
 spec :: Spec
 spec =
   describe "Day18" $ do
+    it "splitPair" $ do
+      let f s es = splitPair s `shouldBe` es
+      f "[[1,2],3]" ("[1,2]", "3")
+      f "[1,2]" ("1", "2")
+      f "[[[0,7],4],[15,[0,13]]]" ("[[0,7],4]", "[15,[0,13]]")
+      f "[[[[0,7],4],[[7,8],[0,13]]],[1,1]]" ("[[[0,7],4],[[7,8],[0,13]]]", "[1,1]")
+      f "[[[0,7],4],[[7,8],[0,13]]]" ("[[0,7],4]", "[[7,8],[0,13]]")
+      f "[[0,7],4]" ("[0,7]", "4")
+      f "[[7,8],[0,13]]" ("[7,8]", "[0,13]")
     it "parse" $ do
       shouldBe
         (parse "[[1,2],3]")
@@ -14,9 +25,16 @@ spec =
       shouldBe
         (toPairRepr $ node (node (leaf 1, leaf 2), leaf 3))
         "[[1,2],3]"
+      shouldBe
+        (toPairRepr $ parse "[[1,2],3]")
+        "[[1,2],3]"
+      shouldBe
+        (toPairRepr . transform leftUnsafe . parse $ "[[1,2],3]")
+        "[1,2]"
     it "toPairRepr . parse" $ do
       let f s = (toPairRepr . parse $ s) `shouldBe` s
       f "[[1,2],3]"
+      f "[[[1,2],3],4]"
       f "[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]"
       f "[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]"
       f "[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]"
@@ -27,3 +45,39 @@ spec =
       f "[1,[[[9,3],9],[[9,0],[0,7]]]]"
       f "[[[5,[7,4]],7],1]"
       f "[[[[4,2],2],6],[8,7]]"
+      f "[[[[0,7],4],[15,[0,13]]],[1,1]]"
+    it "isLeaf" $ do
+      let f s df b = shouldBe (isLeaf . df . fromTree . parse $ s) b
+      f "[[1,2],3]" (leftUnsafe >>> leftUnsafe) True
+      f "[[1,2],3]" (leftUnsafe >>> rightUnsafe) True
+      f "[[1,2],3]" rightUnsafe True
+      f "[[1,2],3]" id False
+      f "[[1,2],3]" leftUnsafe False
+    it "side" $ do
+      let f s df sd = shouldBe (side . df . fromTree . parse $ s) sd
+      f "[[1,2],3]" (leftUnsafe >>> leftUnsafe) LeftSide
+      f "[[1,2],3]" (leftUnsafe >>> rightUnsafe) RightSide
+      f "[[1,2],3]" rightUnsafe RightSide
+      f "[[1,2],3]" id TopSide
+    it "nearestLeft" $ do
+      let f s df es = shouldBe (fmap (toPairRepr . tree) . nearestLeft . df . fromTree . parse $ s) es
+      f "[[1,2],3]" (leftUnsafe >>> leftUnsafe) Nothing
+      f "[[1,2],3]" (leftUnsafe >>> rightUnsafe) (Just "1")
+      f "[[1,2],3]" rightUnsafe (Just "2")
+    it "nearestRight" $ do
+      let f s df es = shouldBe (fmap (toPairRepr . tree) . nearestRight . df . fromTree . parse $ s) es
+      f "[[1,2],3]" (leftUnsafe >>> leftUnsafe) (Just "2")
+      f "[[1,2],3]" (leftUnsafe >>> rightUnsafe) (Just "3")
+      f "[[1,2],3]" rightUnsafe Nothing
+    it "findExplode" $ do
+      let f s es = shouldBe (fmap (toPairRepr . tree) . findExplode . fromTree . parse $ s) es
+      f "[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]" (Just "[4,3]")
+      f "[[[[0,7],4],[7,[[8,4],9]]],[1,1]]" (Just "[8,4]")
+      f "[[[[0,7],4],[15,[0,13]]],[1,1]]" Nothing
+      f "[[[[0,7],4],[[7,8],[0,13]]],[1,1]]" Nothing
+      f "[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]" (Just "[6,7]")
+    xit "explode" $ do
+      let f s es = shouldBe (toPairRepr . tree . snd . explode . fromTree . parse $ s) es
+      f "[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]" "[[[[0,7],4],[7,[[8,4],9]]],[1,1]]"
+      f "[[[[0,7],4],[7,[[8,4],9]]],[1,1]]" "[[[[0,7],4],[15,[0,13]]],[1,1]]"
+      f "[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]" "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"
