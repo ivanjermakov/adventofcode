ed887eca2147dca56e907c6ca7e12c4079518ca4
ivanjermakov <ivanjermakov1@gmail.com>
2022-01-19 20:29:20 +0100

    Day 18.1 parsing

--- src/Day18.hs
+++ src/Day18.hs
@@ -1,6 +1,52 @@
+{-# LANGUAGE TupleSections #-}
+
 module Day18 where
+
+import Data.Char (digitToInt)
+import Data.List (intercalate)
+import Data.Tree
+
+type NTree = Tree (Maybe Int)
 
 main18 :: IO ()
 main18 = do
-  input <- readFile "resources/d16.txt"
-  print $ input
+  input <- readFile "resources/d18-test.txt"
+  let ps = parseInput input
+  print $ map show ps
+
+node :: (NTree, NTree) -> NTree
+node (a, b) = Node Nothing [a, b]
+
+leaf :: Int -> NTree
+leaf v = Node (Just v) []
+
+add :: Tree Int -> Tree Int -> Tree Int
+add a b = Node 0 [a, b]
+
+parseInput :: String -> [NTree]
+parseInput = map parse . lines
+
+parse :: String -> NTree
+parse = unfoldTree f
+  where
+    f :: String -> (Maybe Int, [String])
+    f [n] = (Just $ digitToInt n, [])
+    f s = (Nothing, [tail l, tail . init $ r])
+      where
+        (l, r) = splitAt (midAt (0, 0, s)) s
+
+midAt :: (Int, Int, String) -> Int
+midAt (i, c, '[' : ss) = midAt (i + 1, c + 1, ss)
+midAt (i, c, ']' : ss) = midAt (i + 1, c - 1, ss)
+midAt (i, c, ',' : ss) = if c == 1 then i else midAt (i + 1, c, ss)
+midAt (i, c, _ : ss) = midAt (i + 1, c, ss)
+midAt (i, c, []) = -1
+
+toPairRepr :: NTree -> String
+toPairRepr = foldTree f
+  where
+    f (Just v) _ = show v
+    f Nothing xs = "[" ++ intercalate "," xs ++ "]"
+
+pretty :: NTree -> String
+pretty = drawTree . fmap show
--- test/Day18Spec.hs
+++ test/Day18Spec.hs
@@ -1,8 +1,29 @@
 module Day18Spec where
 
+import Day18
 import Test.Hspec
 
 spec :: Spec
-spec = describe "Day18" $ do
-  it "test" $ do
-    True
+spec =
+  describe "Day18" $ do
+    it "parse" $ do
+      shouldBe
+        (parse "[[1,2],3]")
+        (node (node (leaf 1, leaf 2), leaf 3))
+    it "toPairRepr" $ do
+      shouldBe
+        (toPairRepr $ node (node (leaf 1, leaf 2), leaf 3))
+        "[[1,2],3]"
+    it "toPairRepr . parse" $ do
+      let f s = (toPairRepr . parse $ s) `shouldBe` s
+      f "[[1,2],3]"
+      f "[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]"
+      f "[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]"
+      f "[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]"
+      f "[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]"
+      f "[7,[5,[[3,8],[1,4]]]]"
+      f "[[2,[2,2]],[8,[8,1]]]"
+      f "[2,9]"
+      f "[1,[[[9,3],9],[[9,0],[0,7]]]]"
+      f "[[[5,[7,4]],7],1]"
+      f "[[[[4,2],2],6],[8,7]]"
