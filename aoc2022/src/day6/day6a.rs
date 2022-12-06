17421b0e48ba054a80048ae64a64828e3c077588
ivanjermakov <ivanjermakov1@gmail.com>
2022-12-06 16:15:33 +0100

    Day 6a

---
+++ aoc2022/src/day6/day6a.rs
@@ -0,0 +1,37 @@
+use std::collections::HashSet;
+use std::fs::read_to_string;
+
+pub fn read_input() -> String {
+    read_to_string("data/day6/a.txt").unwrap()
+}
+
+pub fn solve(input: String) -> usize {
+    let window_size = 4;
+    for i in 0..input.len() - window_size {
+        let w = &input[i..i + window_size];
+        if w.chars().collect::<HashSet<_>>().len() == window_size {
+            return i + window_size;
+        }
+    }
+    unreachable!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_solve_example() {
+        assert_eq!(solve("bvwbjplbgvbhsrlpgdmjqwftvncz".to_string()), 5);
+        assert_eq!(solve("nppdvjthqldpwncqszvftbrmjlhg".to_string()), 6);
+        assert_eq!(solve("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg".to_string()), 10);
+        assert_eq!(solve("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".to_string()), 11);
+    }
+
+    #[test]
+    fn should_solve() {
+        let input = read_input();
+        let result = solve(input);
+        assert_eq!(result, 1855);
+    }
+}
