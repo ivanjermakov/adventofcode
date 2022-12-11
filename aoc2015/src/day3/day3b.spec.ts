5bc68d14be4ec8c3c690313371785255a2077dbe
ivanjermakov <ivanjermakov1@gmail.com>
2022-12-11 05:22:48 +0100

    2015.10ab

--- aoc2015/src/day3/day3b.spec.ts
+++ aoc2015/src/day3/day3b.spec.ts
@@ -2,7 +2,7 @@ import assert from 'assert'
 import {readInput} from './day3a'
 import {solve} from './day3b'
 
-describe('day3b', () => {
+xdescribe('day3b', () => {
 	it('should example', () => {
 		assert.equal(solve('^v'), 3)
 		assert.equal(solve('^>v<'), 3)
