96f8ddb3f09df5d9f9755edc07b24e7b5ef07db2
ivanjermakov <ivanjermakov1@gmail.com>
2022-12-11 08:40:21 +0100

    2015.12ab

---
+++ aoc2015/src/day12/day12a.ts
@@ -0,0 +1,12 @@
+import {readFileSync} from 'fs'
+
+export function readInput(): string {
+	return readFileSync('data/day12.txt').toString().trim()
+}
+
+export function solve(input: string): number {
+	const ms = [...input.matchAll(/([+-]?)\d+/g)]
+	const ns = ms.map(s => parseInt(s[0]))
+	console.log(ns)
+	return ns.reduce((a, b) => a + b, 0)
+}
