90bc3e94d7d556d95295eabb0a9743b9e556a812
ivanjermakov <ivanjermakov1@gmail.com>
2023-12-02 06:44:36 +0100

    2023.2 refactoring

--- aoc2023/src/day2/day2b.ts
+++ aoc2023/src/day2/day2b.ts
@@ -1,18 +1,9 @@
+import { getMaps } from "./day2a"
+
 export function solve(input: string): number {
-    const maps = input.split('\n').map(l => {
-        const maps: Map<string, number>[] = []
-        l.split('; ')
-            .forEach(g => {
-                maps.push(new Map())
-                g.split(', ').forEach(p => {
-                    const ws = p.split(' ')
-                    maps.at(-1)!.set(ws[1], (maps.at(-1)!.get(ws[1]) ?? 0) + parseInt(ws[0]))
-                })
-            })
-        return maps
-    })
-    return maps.map((ms, i) => <const>[ms, i])
-        .map(([ms,]) => {
+    const maps = getMaps(input)
+    return maps
+        .map(ms => {
             const mins = ms.reduce(([mR, mG, mB], m) => {
                 mR = Math.max(mR, m.get('red') ?? 0)
                 mG = Math.max(mG, m.get('green') ?? 0)
