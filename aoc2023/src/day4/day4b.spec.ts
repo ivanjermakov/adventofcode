c187159802586dcbe6b5a26a9075c1068ec08cce
ivanjermakov <ivanjermakov1@gmail.com>
2023-12-04 07:01:10 +0100

    2023.4ab

---
+++ aoc2023/src/day4/day4b.spec.ts
@@ -0,0 +1,18 @@
+import { solve } from '../day4/day4b'
+import { readInput } from './day4a'
+
+describe('day4b', () => {
+    it('should solve', () => {
+        expect(solve(readInput())).toEqual(19499881)
+    })
+
+    it('should solve example', () => {
+        expect(solve(`41 48 83 86 17 | 83 86  6 31 17  9 48 53
+13 32 20 16 61 | 61 30 68 82 17 32 24 19
+ 1 21 53 59 44 | 69 82 63 72 16 21 14  1
+41 92 73 84 69 | 59 84 76 51 58  5 54 83
+87 83 26 28 32 | 88 30 70 12 93 22 82 36
+31 18 13 56 72 | 74 77 10 23 35 67 36 11`)).toEqual(30)
+    })
+})
+
