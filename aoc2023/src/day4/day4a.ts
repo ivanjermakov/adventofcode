1ba642149f7fd94a6db8b037095723d976b4ff52
ivanjermakov <ivanjermakov1@gmail.com>
2023-12-05 01:18:31 +0100

    2023.4 cleanup

--- aoc2023/src/day4/day4a.ts
+++ aoc2023/src/day4/day4a.ts
@@ -14,11 +14,12 @@ export function solve(input: string): number {
 }
 
 export function wonCount(card: string): number {
-    const [winning, mine] = card.replaceAll('  ', ' ').split(' | ').map(g => g.split(' ').map(s => Number(s.trim())))
+    const [winning, mine] = card.split('|').map(g => g.trim().split(/\s+/).map(s => parseInt(s)))
     return intersect(mine, winning).length
 }
 
 export function intersect<T>(a: T[], b: T[]): T[] {
-    return [...new Set(a.filter(i => new Set(b).has(i)))];
+    const bSet = new Set(b)
+    return [...new Set(a.filter(e => bSet.has(e)))];
 }
 
