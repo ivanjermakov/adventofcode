786f88087b9c6d283ffa89c37afe9ee2242a2266
ivanjermakov <ivanjermakov1@gmail.com>
2023-12-07 08:25:14 +0100

    2023.7 cleanup

--- aoc2023/src/day7/day7a.ts
+++ aoc2023/src/day7/day7a.ts
@@ -1,5 +1,8 @@
 import { readFileSync } from 'fs'
 
+export const ranks: string[] = '23456789TJQKA'.split('')
+export const hands: string[] = ['high', 'one', 'two', 'three', 'full-house', 'four', 'five']
+
 export type Hand = 'five' | 'four' | 'full-house' | 'three' | 'two' | 'one' | 'high'
 
 export function readInput(): string {
@@ -7,14 +10,14 @@ export function readInput(): string {
 }
 
 export function solve(input: string): number {
-    const gs = input.split('\n').map(l => <const>[l.split(' ')[0], parseInt(l.split(' ')[1])])
+    const gs = input.split('\n').map(l => <const>[l.split(' ')[0].split(''), parseInt(l.split(' ')[1])])
     gs.sort((g1, g2) => {
-        const o1 = ordHand(getHand(g1[0].split('')))
-        const o2 = ordHand(getHand(g2[0].split('')))
+        const o1 = hands.indexOf(getHand(g1[0]))
+        const o2 = hands.indexOf(getHand(g2[0]))
         if (o1 == o2) {
-            for (let i = 0; i < g1[0].split('').length; i++) {
-                let c1 = ordCard(g1[0].split('')[i])
-                let c2 = ordCard(g2[0].split('')[i])
+            for (let i = 0; i < g1[0].length; i++) {
+                let c1 = ranks.indexOf(g1[0][i])
+                let c2 = ranks.indexOf(g2[0][i])
                 if (c1 !== c2) return c1 - c2
             }
             return 0
@@ -29,45 +32,19 @@ export function getHand(cs: string[]): Hand {
     for (let c of cs) {
         dist.set(c, dist.get(c) ? dist.get(c)! + 1 : 1)
     }
-    const has5 = [...dist.values()].some(d => d === 5)
-    const has4 = [...dist.values()].some(d => d === 4)
+    if ([...dist.values()].some(d => d === 5)) return 'five'
+    if ([...dist.values()].some(d => d === 4)) return 'four'
     const has3 = [...dist.values()].some(d => d === 3)
-    const has2p = [...dist.values()].filter(d => d === 2).length === 2
     const has2 = [...dist.values()].some(d => d === 2)
-    if (has5) return 'five'
-    if (has4) return 'four'
     if (has3 && has2) return 'full-house'
     if (has3) return 'three'
-    if (has2p) return 'two'
+    if ([...dist.values()].filter(d => d === 2).length === 2) return 'two'
     if (has2) return 'one'
     return 'high'
 }
 
-export function ordHand(h: Hand): number {
-    switch (h) {
-        case 'five': return 7
-        case 'four': return 6
-        case 'full-house': return 5
-        case 'three': return 4
-        case 'two': return 3
-        case 'one': return 2
-        case 'high': return 1
-    }
-}
-
-export function ordCard(c: string): number {
-    switch (c) {
-        case 'A': return 14
-        case 'K': return 13
-        case 'Q': return 12
-        case 'J': return 11
-        case 'T': return 10
-        default: return parseInt(c)
-    }
-}
-
 export function high(cs: string[]): string {
     const cp = [...cs]
-    cp.sort((a, b) => ordCard(b) - ordCard(a))
+    cp.sort((a, b) => ranks.indexOf(b) - ranks.indexOf(a))
     return cp[0]
 }
