3314f3e68d738d0ccfc31cbdbfa5b33254164f1d
ivanjermakov <ivanjermakov1@gmail.com>
2023-12-08 06:54:07 +0100

    2023.8 cleanup

--- aoc2023/src/day8/day8b.ts
+++ aoc2023/src/day8/day8b.ts
@@ -1,20 +1,10 @@
-import { parse } from "./day8a"
+import { countSteps, parse } from "./day8a"
 
 export function solve(input: string): number {
     const { moves, map } = parse(input)
-    let ps = [...map.keys()].filter(k => k.endsWith('A'))
-    let ttz: number[] = new Array(ps.length).fill(0)
-    for (let n = 0; n < ps.length; n++) {
-        for (let i = 0; ; i++) {
-            const m = moves[i % moves.length]
-            ps[n] = map.get(ps[n])![m === 'L' ? 0 : 1]
-            if (ps[n].endsWith('Z')) {
-                ttz[n] = i + 1
-                break
-            }
-        }
-    }
-    return ttz.reduce((a, b) => leastCommonMultiple(a, b), 1)
+    return [...map.keys()].filter(k => k.endsWith('A'))
+        .map(p => countSteps(p, moves, map))
+        .reduce((a, b) => leastCommonMultiple(a, b), 1)
 }
 
 export function leastCommonMultiple(a: number, b: number): number {
