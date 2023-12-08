0eb81b85360d4f961459f2890aaa61a2d047b34e
ivanjermakov <ivanjermakov1@gmail.com>
2023-12-08 06:54:07 +0100

    2023.8 cleanup

--- aoc2023/src/day8/day8a.ts
+++ aoc2023/src/day8/day8a.ts
@@ -6,22 +6,22 @@ export function readInput(): string {
 
 export function solve(input: string): number {
     const { moves, map } = parse(input)
-    let pos = 'AAA'
+    return countSteps('AAA', moves, map)
+}
+
+export function countSteps(pos: string, moves: string[], map: Map<string, string[]>): number {
     for (let i = 0; ; i++) {
         const m = moves[i % moves.length]
         pos = map.get(pos)![m === 'L' ? 0 : 1]
-        if (pos === 'ZZZ') {
+        if (pos.endsWith('Z')) {
             return i + 1
         }
     }
 }
 
 export function parse(input: string): { moves: string[]; map: Map<string, string[]> } {
-    const [moves_, map_] = input.split('\n\n')
+    const [moves_, map_] = input.replaceAll(/= \(|,|\)/g, '').split('\n\n')
     const moves = moves_.split('')
-    const map = new Map(map_.split('\n').map(l => {
-        const ns = [...l.matchAll(/[A-Z]+/g)].map(m => m[0])
-        return [ns[0], ns.slice(1)]
-    }))
+    const map = new Map(map_.split('\n').map(l => l.split(' ')).map(l => [l[0], l.slice(1)]))
     return { moves, map }
 }
