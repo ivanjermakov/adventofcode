9350221f6aa114cff0d26abaf75cd26ceaeb1400
ivanjermakov <ivanjermakov1@gmail.com>
2023-12-08 06:35:37 +0100

    2023.8 cleanup

--- aoc2023/src/day8/day8a.ts
+++ aoc2023/src/day8/day8a.ts
@@ -5,20 +5,23 @@ export function readInput(): string {
 }
 
 export function solve(input: string): number {
-    const inp = input.split('\n\n')
-    const moves = inp[0].split('')
-    const map = new Map(inp[1].split('\n').map(l => {
-        const v = l.split('=')[1].split(', ')
-        return [l.split(' ')[0], [v[0].slice(2), v[1].slice(0, -1)]]
-    }))
-    let i = 0
+    const { moves, map } = parse(input)
     let pos = 'AAA'
-    while (true) {
+    for (let i = 0; ; i++) {
         const m = moves[i % moves.length]
         pos = map.get(pos)![m === 'L' ? 0 : 1]
         if (pos === 'ZZZ') {
             return i + 1
         }
-        i++
     }
+}
+
+export function parse(input: string): { moves: string[]; map: Map<string, string[]> } {
+    const [moves_, map_] = input.split('\n\n')
+    const moves = moves_.split('')
+    const map = new Map(map_.split('\n').map(l => {
+        const ns = [...l.matchAll(/[A-Z]+/g)].map(m => m[0])
+        return [ns[0], ns.slice(1)]
+    }))
+    return { moves, map }
 }
--- aoc2023/src/day8/day8b.ts
+++ aoc2023/src/day8/day8b.ts
@@ -1,22 +1,17 @@
+import { parse } from "./day8a"
+
 export function solve(input: string): number {
-    const inp = input.split('\n\n')
-    const moves = inp[0].split('')
-    const map = new Map(inp[1].split('\n').map(l => {
-        const v = l.split('=')[1].split(', ')
-        return [l.split(' ')[0], [v[0].slice(2), v[1].slice(0, -1)]]
-    }))
+    const { moves, map } = parse(input)
     let ps = [...map.keys()].filter(k => k.endsWith('A'))
     let ttz: number[] = new Array(ps.length).fill(0)
     for (let n = 0; n < ps.length; n++) {
-        let i = 0
-        while (true) {
+        for (let i = 0; ; i++) {
             const m = moves[i % moves.length]
             ps[n] = map.get(ps[n])![m === 'L' ? 0 : 1]
             if (ps[n].endsWith('Z')) {
                 ttz[n] = i + 1
                 break
             }
-            i++
         }
     }
     return ttz.reduce((a, b) => leastCommonMultiple(a, b), 1)
