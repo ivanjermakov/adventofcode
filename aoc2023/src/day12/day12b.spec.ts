c15bfd748b6c1561e61fb67a45d53cdfd3e2af21
ivanjermakov <ivanjermakov1@gmail.com>
2023-12-12 19:01:28 +0100

    2023.12b

--- aoc2023/src/day12/day12b.spec.ts
+++ aoc2023/src/day12/day12b.spec.ts
@@ -1,12 +1,17 @@
 import { solve } from '../day12/day12b'
 import { readInput } from './day12a'
 
-describe.skip('day12b', () => {
-    it('should solve', () => {
-        expect(solve(readInput())).toEqual(0)
+describe('day12b', () => {
+    it.skip('should solve', () => {
+        expect(solve(readInput(), 5)).toEqual(0)
     })
     it('should solve example', () => {
-        expect(solve(``)).toEqual(0)
+        expect(solve(`???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1`, 5)).toEqual(525152)
     })
 })
 
--- aoc2023/src/day12/day12b.ts
+++ aoc2023/src/day12/day12b.ts
@@ -1,5 +1,23 @@
-import { todo } from "../util";
+import { parse, solveRow } from './day12a'
 
-export function solve(input: string): number {
-    return todo()
+export function solve(input: string, copies: number): number {
+    return input
+        .split('\n')
+        .map(l => {
+            const [row, groups] = parse(l)
+            return <const>[repeat(row, copies, '?'), repeat(groups, copies)]
+        })
+        .map(([row, groups]) => solveRow(row, groups))
+        .reduce((a, b) => a + b, 0)
+}
+
+export function repeat<T>(a: T[], n: number, separator?: T): T[] {
+    const res = []
+    for (let i = 0; i < n; i++) {
+        res.push(...a)
+        if (separator && i !== n - 1) {
+            res.push(separator)
+        }
+    }
+    return res
 }
