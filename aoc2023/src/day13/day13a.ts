c840d5b97d8365940a02f042e43bc04cb17f6d5a
ivanjermakov <ivanjermakov1@gmail.com>
2023-12-13 18:50:53 +0100

    2023.13b

--- aoc2023/src/day13/day13a.ts
+++ aoc2023/src/day13/day13a.ts
@@ -1,6 +1,5 @@
 import { readFileSync } from 'fs'
-import { printGrid, transpose } from '../day11/day11a'
-import { unreachable } from '../util'
+import { transpose } from '../day11/day11a'
 
 export function readInput(): string {
     return readFileSync('data/day13.txt').toString().trim()
@@ -10,23 +9,24 @@ export function solve(input: string): number {
     return input
         .split('\n\n')
         .map(g => g.split('\n').map(l => l.split('')))
-        .map(symmetryScore)
+        .map(g => symmetryScores(g)[0])
         .reduce((a, b) => a + b, 0)
 }
 
-export function symmetryScore(grid: string[][]): number {
+export function symmetryScores(grid: string[][]): number[] {
+    const ss = []
     for (let r = 0; r < grid.length - 1; r++) {
         if (isSymmetrical(r, grid)) {
-            return (r + 1) * 100
+            ss.push((r + 1) * 100)
         }
     }
     const gridTr = transpose(grid)
     for (let c = 0; c < gridTr.length - 1; c++) {
         if (isSymmetrical(c, gridTr)) {
-            return c + 1
+            ss.push(c + 1)
         }
     }
-    return unreachable('no symmetry')
+    return ss
 }
 
 export function isSymmetrical(i: number, grid: string[][]): boolean {
