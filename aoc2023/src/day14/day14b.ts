520c4f59bcbfee1a55ccd527268c6ca4d6f4482c
ivanjermakov <ivanjermakov1@gmail.com>
2023-12-14 11:56:51 +0100

    2023.14b optimize

--- aoc2023/src/day14/day14b.ts
+++ aoc2023/src/day14/day14b.ts
@@ -1,13 +1,21 @@
-import { roll, score } from "./day14a"
+import { roll } from "./day14a"
 
 const cache = new Map<string, string>()
 
-export function solve(input: string, lim: number = 1_000_000_000): number {
+export function solve(input: string, iter: number = 1_000_000_000): number {
     let g = input
-    for (let n = 0; n < lim; n++) {
+    const keys: string[] = []
+    for (let n = 0; n < iter; n++) {
         g = rollCached(g)
+        const i = keys.indexOf(g)
+        if (i !== -1) {
+            const cycle = keys.slice(i)
+            const finalKey = cycle[(iter - i - 1) % cycle.length]
+            return score(finalKey)
+        }
+        keys.push(g)
     }
-    return score(g.split('\n').map(r => r.split('')))
+    return score(g)
 }
 
 export function rollCached(input: string): string {
@@ -24,4 +32,21 @@ export function rollCached(input: string): string {
     const res = g.flatMap(r => r.join('')).join('\n')
     cache.set(key, res)
     return res
+}
+
+export function freq<T>(a: T[]): Map<T, number> {
+    const m = new Map()
+    for (const e of a) {
+        const old = m.get(e)
+        m.set(e, old ? old + 1 : 1)
+    }
+    return m
+}
+
+export function score(input: string): number {
+    const grid = input
+        .split('\n').map(r => r.split(''))
+    return grid
+        .map((r, i) => r.filter(e => e === 'O').length * (grid.length - i))
+        .reduce((a, b) => a + b, 0)
 }
