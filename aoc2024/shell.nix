0db6eb00686b6cf89566cc98a15e1b8d3d1ec96a
ivanjermakov <ivanjermakov1@gmail.com>
2024-12-01 17:27:56 +0100

    2024.1a parse file

--- aoc2024/shell.nix
+++ aoc2024/shell.nix
@@ -1,6 +1,8 @@
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell {
+    LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
     nativeBuildInputs = with pkgs; [
+        nasm
     ];
 }
