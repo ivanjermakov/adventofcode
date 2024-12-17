26ff2db7aa7f9d4e82e24b77d1ee0ec2a496c3ce
ivanjermakov <ivanjermakov1@gmail.com>
2024-12-17 23:30:14 +0100

    2024.15b

--- aoc2024/src/day15/day15b.ml
+++ aoc2024/src/day15/day15b.ml
@@ -17,60 +17,35 @@ let parse input = match Str.split (Str.regexp_string "\n\n") input with
                 fun i row -> row
                     |> String.to_seq
                     |> List.of_seq
-                    |> List.mapi (fun j v -> ((i, j * 2), v))
+                    |> List.mapi (fun j v ->
+                        match v with
+                        | 'O' -> [((i, j * 2), '['); ((i, j * 2 + 1), ']')]
+                        | '#' -> [((i, j * 2), v); ((i, j * 2 + 1), v)]
+                        | _ -> [((i, j * 2), v)]
+                    )
+                    |> List.concat
             )
             |> List.concat in
-        let (p, _) = ps |> List.find (fun (_, v) -> v = '@') in
         (
             List.filter (fun (_, v) -> v != '.') ps,
-            p,
+            List.find (fun (_, v) -> v = '@') ps,
             dirs |> String.to_seq |> Seq.filter (fun c -> c != '\n')
         )
     | _ -> assert false
 
-let rec move_ cells pos dir = 
-    let at p = List.find_opt (fun (p, _) -> p = pos) in
-    let (_, v) = match at pos cells with | Some c -> c | _ -> assert false in
-    let npos = let (i, j) = pos in
-    match dir with
-        | '^' -> (i - 1, j)
-        | 'v' -> (i + 1, j)
-        | '<' -> (i, j - 1)
-        | '>' -> (i, j + 1)
-        | _ -> assert false in
-    let other_cells p = List.filter (fun (p_, _) -> p_ <> p) in
-    let left (i, j) = (i, j - 1) in
-    let right (i, j) = (i, j + 1) in
-    let collider_origins = match (dir, v) with
-        | ('>', '@') -> [npos; right npos]
-        | ('<', '@') -> [npos; left npos]
-        | (_, '@') -> [npos; left npos]
-        | ('>', 'O') -> [right npos]
-        | ('<', 'O') -> [left npos]
-        | (_, 'O') -> [npos; left npos; right npos]
-        | _ -> assert false in
-    let colliders = collider_origins
-        |> List.map (fun c -> at c cells)
-        |> all_some
-    in
-    match colliders with
-    | Some cs -> raise Todo
-    | _ -> Some ((other_cells pos cells) @ [(npos, v)], npos)
+let rec move_ cells cell dir = 
+    Some (cells, cell)
 
-let move cells pos dir = match move_ cells pos dir with
+let move cells cell dir = match move_ cells cell dir with
     | Some r -> r
-    | _ -> (cells, pos)
+    | _ -> (cells, cell)
 
 let range l = List.init l (fun i -> i)
 
 let show_cells cs =
     let size = 7 in
-    let at i j = List.find_opt (fun ((ci, cj), _) -> ci = i && cj = j) in
-    let show_cell i j = match (cs |> at i j, cs |> at i (j - 1)) with
-    | (Some (_, '@'), _) -> '@'
-    | (Some (_, 'O'), _) -> '['
-    | (_, Some (_, 'O')) -> ']'
-    | (Some (_, '#'), _) | (_, Some (_, '#')) -> '#'
+    let show_cell i j = match cs |> List.find_opt (fun ((ci, cj), _) -> ci = i && cj = j) with
+    | Some (_, c) -> c
     | _ -> '.' in
     let join chs = chs |> List.map (String.make 1) in
     range size
@@ -79,15 +54,15 @@ let show_cells cs =
     |> String.concat "\n"
 
 let dist ((i, j), v) = match v with
-| 'O' -> 100 * i + j
+| '[' -> 100 * i + j
 | _ -> 0
 
 let solve =
     let input = read_file "data/day15.txt" in
-    let (cells, pos, dirs) = parse input in
+    let (cells, cell, dirs) = parse input in
     let res = dirs
     |> Seq.take 1
-    |> Seq.fold_left (fun (cs, ps) d -> move cs ps d) (cells, pos)
+    |> Seq.fold_left (fun (cs, ps) d -> move cs ps d) (cells, cell)
     |> fst in
     print_endline (show_cells res);
     (* res *)
