0a73b04c43b300db0dcd00e52d6076d936a78c8e
ivanjermakov <ivanjermakov1@gmail.com>
2025-12-01 10:55:35 +0100

    2025 setup

--- aoc2025/build.zig
+++ aoc2025/build.zig
@@ -4,13 +4,14 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const mod = b.createModule(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
     const exe = b.addExecutable(.{
         .name = "aoc2025",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-        }),
+        .root_module = mod,
     });
 
     b.installArtifact(exe);
@@ -26,12 +27,16 @@ pub fn build(b: *std.Build) void {
         run_cmd.addArgs(args);
     }
 
-    const exe_tests = b.addTest(.{
+    const tests = b.addTest(.{
         .root_module = exe.root_module,
     });
 
-    const run_exe_tests = b.addRunArtifact(exe_tests);
+    const run_tests = b.addRunArtifact(tests);
 
     const test_step = b.step("test", "Run tests");
-    test_step.dependOn(&run_exe_tests.step);
+    test_step.dependOn(&run_tests.step);
+
+    const check_step = b.step("check", "");
+    check_step.dependOn(&exe.step);
+    check_step.dependOn(&tests.step);
 }
--- aoc2025/src/main.zig
+++ aoc2025/src/main.zig
@@ -1,7 +1,12 @@
 const std = @import("std");
+const day1 = @import("day1.zig");
 
 pub fn main() !void {
     std.debug.print("Hello, AOC!\n", .{});
+}
+
+test {
+    std.testing.refAllDeclsRecursive(@This());
 }
 
 test "test" {}
